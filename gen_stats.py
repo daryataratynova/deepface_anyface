0c7124f665eb3fe09a940e44531bfb6ccfd53fe9
Darya Taratynova <darya.taratynova@Daryas-MacBook-Air-2.local>
2023-06-28 12:59:50 +0600

    results

--- gen_stats.py
+++ gen_stats.py
@@ -1,96 +1,38 @@
 import pandas as pd
 import argparse
 
-#returns percentage of each group in undetected
-def ratio_in_undetected(data, col):
-    col_classes = data.groupby(col)[col].count()
-    return round(col_classes/data[col].count()*100, 2)
+def read_file(file_name):
+    return pd.read_csv(file_name)
 
-#returns percentage of each group in total
-def ratio_in_total(data, total_data, col):
-     col_classes = data.groupby(col)[col].count()
-     print(total_data.groupby(col)[col].count())
-     return round(col_classes/total_data.groupby(col)[col].count()*100, 2)
+def count_groups(df, group):
+    return df[group].value_counts(sort=False).rename().to_frame().reset_index().sort_values(by='index')
 
-#returns ratio of undetected/total
-def ratio(data, total_data):
-     return round(data.shape[0]/total_data.shape[0]*100, 2)
+def gen_csv(folders, file_name, groups):
+    for group in groups:
+        result = pd.DataFrame()
 
-def read_files(model):
-     train = pd.read_csv("fairface/fairface_label_train.csv")
-     val = pd.read_csv("fairface/fairface_label_val.csv")
+        for folder in folders:
+            file_path = folder + "/" + file_name
+            df = read_file(file_path)
 
-     #choose rows with service_test = True to get balanced dataset
-     train = train[(train['service_test'] == True)] 
-     val = val[(val['service_test'] == True)] 
+            count = count_groups(df, group)
 
-     #read undetected faces 
-     undetected_train = pd.read_csv("fairface/"+ model + "/train_balanced1280.csv")
-     undetected_val = pd.read_csv("fairface/"+ model  + "/val_balanced1280.csv") 
+            if group not in result.columns:
+                result[group] = count['index']
+            
+            result[folder] = count[0].values  # Use .values to assign the counts based on position instead of index alignment
+            
+        result.to_csv("results/" + f"{group}_result.csv", index=False)
 
-     return undetected_train, train, undetected_val, val
-
-#generate csv file
-def gen_csv(param, models):
-     train_df, val_df, undetected_percentage_train, undetected_percentage_val = pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
-     undetected_number_train, undetected_number_val = pd.DataFrame(),pd.DataFrame()
-     param_data_status = False
-     i = 1
-
-     
-     #for each model store result 
-     for model in models:
-
-          #read files with undetected faces
-          undetected_train, train, undetected_val, val = read_files(model)
-
-          ratio_in_total(undetected_train, train, param)
-          ratio_in_total(undetected_val, val, param )
-
-          res_train = ratio_in_undetected(undetected_train, param).rename().to_frame().reset_index()
-          res_val = ratio_in_undetected(undetected_val, param).rename().to_frame().reset_index()
-          
-          number_train = undetected_train[param].value_counts(sort = False).rename().to_frame().reset_index()
-          number_val = undetected_val[param].value_counts(sort = False).rename().to_frame().reset_index()
-          if param_data_status == False:
-               train_df.insert(0, param, res_train[param])
-               val_df.insert(0, param, res_val[param])
-               undetected_number_train.insert(0, param, number_train['index'])
-               undetected_number_val.insert(0, param, number_val['index'])
-               param_data_status = True
-          
-
-          res_train.columns = [param, '%']
-          res_val.columns = [param, '%']
-
-          train_df.insert(i, model, res_train["%"])
-          val_df.insert(i, model, res_val["%"])
-          
-         
-          undetected_number_train.insert(i, model, number_train[0])
-          undetected_number_val.insert(i, model, number_val[0])
-          ratio_train = ratio(undetected_train, train)
-          ratio_val = ratio(undetected_val, val)
-          undetected_percentage_train = undetected_percentage_train.append({model : ratio_train}, ignore_index =True)
-          undetected_percentage_val = undetected_percentage_val.append({model : ratio_val}, ignore_index = True)
-
-          
-          i+=1
-
-     train_df.to_csv("results_csv/" + param + "_train.csv")
-     val_df.to_csv("results_csv/" + param + "_val.csv")
-     undetected_number_train.to_csv("results_csv/" + param + "_number_train.csv")
-     undetected_number_val.to_csv("results_csv/" + param + "_number_val.csv")
-     undetected_percentage_train.to_csv("results_csv/" + "undetected_percentage_train.csv")
-     undetected_percentage_val.to_csv("results_csv/" + "undetected_percentage_val.csv")
 if __name__ == '__main__':
-     columns = ["age", "gender", "race"]
-     models = [ "anyface"]
-     #, "Yolov5l", "retinaface", "opencv", "ssd", "mtcnn", "dlib"
-     parser = argparse.ArgumentParser()
-     parser.add_argument('--model_name', type=str, default = 'retinaface', help = 'model name')
-     parser.add_argument('--stats_format', type=str, default = 'csv', help = 'model name')
-     parser.add_argument('--param', type = str, default = 'race', help = 'age, gender or race')
-     opt = parser.parse_args()
+    groups = ["age", "gender", "race"]
+    folders = [ "fairface/retinaface", "fairface/opencv", "fairface/dlib", "fairface/ssd", "fairface/mtcnn"]
+    file_name = "train_balanced.csv"
 
-     gen_csv(opt.param, models)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--folder_names', type=str, nargs='+', default = folders, help = 'folder names')
+    parser.add_argument('--file_name', type=str, default = file_name, help = 'file name')
+    parser.add_argument('--groups', type=str, nargs='+', default = groups, help = 'age, gender or race')
+    opt = parser.parse_args()
+
+    gen_csv(opt.folder_names, opt.file_name, opt.groups)
